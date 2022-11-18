691cd2e52316fb071e0ebacbaffd1b004ff9aac9
xiyu1984 <shanellyy@126.com>
2022-11-18 13:23:22 +0800

    update init sh

--- op.sh
+++ op.sh
@@ -5,10 +5,15 @@ flow project deploy --update -n testnet
 
 flow project deploy --update
 
+## Generate Alice
 flow accounts create --key 81262aa27f1630ccf1293300e8e1d9a6ba542dffa796b860d53873867175e9d31bd7b7581d2f200f9c3dfdbc10ae912ff036946981e3d8996a14f186d20e3e2f
-
-# transfer Flow Token
+### transfer Flow Token
 flow transactions send ./transactions/test/transferFlow.cdc 100.0 0x01cf0e2f2f715450
+
+## Generate Bob
+flow accounts create --key 7bfb71eeda6d501caa816044b3382cec7d57e423baee3e02bb47a4420c149fbde0c43ff947eedbdc8740a29f66c7a04add27fe6db1e2976e50e1b2abf302da42
+### transfer Flow Token
+flow transactions send ./transactions/test/transferFlow.cdc 100.0 0x179b6b1cb6755e31
 
 
 flow signatures generate "hello nika" --signer emulator-account
@@ -136,6 +141,6 @@ flow transactions send ./transactions/send-recv-message/trigger.cdc
 flow scripts execute ./scripts/send-recv-message/queryHistory.cdc
 
 # SQoS
-flow transactions send ./transactions/SQoS/setSQoS.cdc 
+flow transactions send ./transactions/SQoS/setOptimistic.cdc 
 
 flow scripts execute ./scripts/SQoS/getSQoS.cdc 0xf8d6e0586b0a20c7
